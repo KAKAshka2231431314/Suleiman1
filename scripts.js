40f87a03095867a000e21fecf054c5bbe1fd7f2b
KAKAshka2231431314 <122794407+KAKAshka2231431314@users.noreply.github.com>
2023-01-16 17:23:32 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+//1
+//let num = (num === 5)?true:false;
+//2
+//let min=+prompt("Введите число от 0 до 59");
+    //if(min>= 0 && min <= 14){
+        //console.log("Первая");
+    //}
+    //else if (min >= 15 && min <= 30){
+        //console.log ("Вторая");
+    //}
+    //else if (min >= 31 && min <= 45){
+        //console.log("Третья");
+    //}
+    //else if (min >=46 && min <= 59) {
+        //console.log("Четвертая");
+    //}
+//3
+//var lang ='ru' ;
+//if  (lang == 'ru') {
+     //var arr = ["пн","вт","ср","чт","пт","cб","вс"];
+//} 
+//else if(lang=="en"){
+    //arr = ["mn","ts","wd","th","fr","st","sn"]
+//};
+//console.log(arr);
+//4
+
+//5
+//let i = 1;
+// while(i <= 12){
+//     console.log(i);
+//     i++;
+//6
+//let i = 5;
+// while(i <= 13){
+//     console.log(i);
+//     i++;
+//7
+//for (let i = 0; i <= 16; i++) {
+    //if(i % 2== 0);
+    //console.log(i+'<br>');
+//}
+//8
+//let array = [1,2,3,4,5];
+//let result = 0;
+//for (let i = 0; i < array.lenght; i++){
+    //result +=array[i];
+//}
+//console.log(result);
+
+    
